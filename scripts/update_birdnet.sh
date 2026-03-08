8278430d5bec32a2a4327b93358eeb9c1e171b35
simogogo <stefanetti@erweb.it>
2026-03-08 22:28:33 +0100

    Update update_birdnet.sh

--- scripts/update_birdnet.sh
+++ scripts/update_birdnet.sh
@@ -87,4 +87,9 @@ sudo ln -sf $my_dir/* /usr/local/bin/
 
 # The script below handles changes to the host system
 # Any additions to the updater should be placed in that file.
+sudo $my_dir/update_caddyfile.sh
 sudo $my_dir/update_birdnet_snippets.sh
+
+if [ ! -e ${EXTRACTED}/api_v2.php ];then
+  sudo_with_user ln -fs $HOME/BirdNET-Pi/scripts/api_v2.php ${EXTRACTED}
+fi
