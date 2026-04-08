d2e1ed2c6ecf254288cd9cbb0159be7ff0f83b91
simogogo <stefanetti@erweb.it>
2026-04-08 23:45:39 +0200

    finetuning

--- homepage/flutter_bootstrap.js
+++ homepage/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1771642192" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2390253841" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
